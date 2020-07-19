5abe3036cf5ae4d8f230d8becbaca852c769848a
Levon Barsegjan <levon.barsegjan@parkit.cloud>
2020-07-19 10:07:35 +0100

    Committing build files for cPanel deployment purposes

---
+++ build/precache-manifest.4b5858fba12f52cf6e8a3c5adf202e88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e7afd89e69c1252abc00d8be45ec5e0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c9b886a9e7089049052c",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6b2b0b1084234f91ea39",
+    "url": "/static/js/2.ca58a160.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ca58a160.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9b886a9e7089049052c",
+    "url": "/static/js/main.93b0d4e8.chunk.js"
+  },
+  {
+    "revision": "27235aa1c33fd56814e2",
+    "url": "/static/js/runtime-main.d4e1c521.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
